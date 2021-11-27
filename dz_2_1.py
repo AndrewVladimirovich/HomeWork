a87bf1e10e32509b0fe0d7ee47985242a88cf402
Tretyakov Andrey <94076226+AndrewVladimirovich@users.noreply.github.com>
2021-11-27 17:55:35 +0300

    Add files via upload

---
+++ dz_2_1.py
@@ -0,0 +1,20 @@
+my_expression = [15*3, 15/3, 15//2, 15*2]
+# Выполняем через цикл for in
+for i in my_expression:
+    print(type(i))
+
+# И если совсем по простому то можно так
+my_expression_1 = 15*3
+print(type(my_expression_1))
+
+my_expression_2 = 15/3
+print(type(my_expression_2))
+
+my_expression_3 = 15//2
+print(type(my_expression_3))
+
+my_expression_4 = 15**2
+print(type(my_expression_4))
+
+
+
